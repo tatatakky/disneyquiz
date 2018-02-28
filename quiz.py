319043d2f0e23b6cc8a6f48296597e37455acd95
tatatakky <s1240232@gmail.com>
2018-02-28 16:20:51 +0900

    2nd heroku deploy [SendImageTest]

--- quiz.py
+++ quiz.py
@@ -1,19 +1,6 @@
 import linecache,random
-def IMG(data):
-    return data[6]
-def Question(data):
-    return data[0]
-def Choices_1(data):
-    return data[1]
-def Choices_2(data):
-    return data[2]
-def Choices_3(data):
-    return data[3]
-def Choices_4(data):
-    return data[4]
 def Solve(text,data):
-    ans = data[5]
-    if ans == text:
+    if data == text:
         return "正解だよ！！"
     else:
         return "残念。正解は{}だよ！".format(ans)
