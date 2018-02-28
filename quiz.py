72ce2dcf26dfb6b51ef9dcb7312ef233dad5b5cd
tatatakky <s1240232@gmail.com>
2018-02-28 16:22:25 +0900

    2nd heroku deploy [SendImageTest]

--- quiz.py
+++ quiz.py
@@ -3,7 +3,7 @@ def Solve(text,data):
     if data == text:
         return "正解だよ！！"
     else:
-        return "残念。正解は{}だよ！".format(ans)
+        return "残念。正解は{}だよ！".format(text)
 # import random
 # def random_image():
 #     lines=[line.replace('\n','') for line in open('jeongyeon_image.txt','r')]
