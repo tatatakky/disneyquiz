cd3e6e6fa36e6dc0e9fd4e4891eb76ce508573a7
tatatakky <s1240232@gmail.com>
2018-03-05 22:32:45 +0900

    [add] increase quiz and add code

--- quiz.py
+++ quiz.py
@@ -12,7 +12,7 @@ def Solve(text,D):
     else:
         return "残念。正解は{}だよ！".format(D[-1:]), 131
 def Usage():
-    text="ボタンを押してみよう！\n\nリトルグリーンメン: Quizができるよ！\n\nミニー: ディズニー公式のtwitterが見られるよ！\n\n プーさん: ディズニーの公式サイトが見られるよ！\n\nミッキーの手: 運勢を占えるよ！\n\nサリー＆マイク: ディズニー公式のInstagramが見られるよ！"
+    text="ボタンを押してみよう！\n\n<リトルグリーンメン>\nQuizができるよ！\n\n<ミニー>\nディズニー公式のtwitterが見られるよ！\n\n<プーさん>\nディズニーの公式サイトが見られるよ！\n\n<ミッキーの手>\nアトラクションを選んでくれるよ！\n\n<サリー＆マイク>\nディズニー公式のInstagramが見られるよ！\n"
     return text
 
 # import random
