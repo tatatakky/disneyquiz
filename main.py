44a0761f89d560e52a5dfa7a945efbd72d2b5fe0
tatatakky <s1240232@gmail.com>
2018-03-07 00:48:02 +0900

    [Fix] server grammer and add func

--- main.py
+++ main.py
@@ -151,10 +151,10 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token,
         [
         TextSendMessage(text="今日のラッキーディズニーキャラクターは、、、"),
-        TextSendMessage(text=info[0]+"だよ！！")
+        TextSendMessage(text=info[0]+"だよ！！"),
         ImageSendMessage(
-            original_content_url=info[1],
-            preview_image_url=info[1])
+        original_content_url=info[1],
+        preview_image_url=info[1])
         ])
         flag=0
         count=0
